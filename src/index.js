2726b892cbbab538fa8b2df3d5066af6fc2c4139
João Arouca <joao.silveira@estudante.ufla.br>
2023-01-02 16:56:03 -0300

    chore(dotenv): install dotenv and create .env file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const app = require('./app');
-const db = require('./database/models/index');
+// const db = require('./database/models/index');
+require('dotenv').config();
 
-const PORT = 3001;
+const PORT = process.env.APP_PORT
 
 app.listen(PORT,  async () => {
     // await db.sync({ force: true }); // Force sync database
-    await db.sequelize.sync({ alter: true }); // Force sync database
+    // await db.sequelize.sync({ alter: true }); // Force sync database
 
     console.log(`Aplicação rodando na porta ${PORT}`);
 });
