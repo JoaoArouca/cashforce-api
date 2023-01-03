3343d59a260e36f3a0c1b05aa8681672f63b28a2
JoaoArouca <joao.silveira@estudante.ufla.br>
2023-01-03 12:30:27 -0300

    fix(lint): fix linter issues

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-const app = require('./app');
-// const db = require('./database/models/index');
 require('dotenv').config();
+const app = require('./app');
 
-const PORT = process.env.APP_PORT
+const PORT = process.env.APP_PORT;
 
 app.listen(PORT,  async () => {
-    // await db.sync({ force: true }); // Force sync database
-    // await db.sequelize.sync({ alter: true }); // Force sync database
-
     console.log(`Aplicação rodando na porta ${PORT}`);
 });
