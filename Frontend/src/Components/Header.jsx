19dde888a69cd7e8397b50a1672a79cfdd67168b
JoaoArouca <joao.silveira@estudante.ufla.br>
2023-01-04 13:05:02 -0300

    feat(front) initial frontend setup -> react + Vite

---
+++ Frontend/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ReactComponent as Logo } from '../../public/Framelogo.svg';
+
+function Header() {
+    return (
+        <header>
+            <Logo />
+        </header>
+    );
+}
+
+export default Header;
